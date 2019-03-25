eb7cb3174e0596312d9a0427b023205161e050ed
gghosth <42755106+gghosth@users.noreply.github.com>
2019-03-25 13:46:46 -0300

    Adicionando base do projeto

---
+++ DarkSoulsBuilds/Armas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DarkSoulsBuilds
+{
+    class Armas
+    {
+
+        private int dano;
+        private int strmin;
+        private int dexmin;
+
+
+
+        public void ondeDevoEstar()
+        {
+
+            Console.WriteLine("Devo estar aqui!");
+
+
+        }
+
+
+
+    }
+}
---
+++ DarkSoulsBuilds/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DarkSoulsBuilds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Armas arma = new Armas();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
